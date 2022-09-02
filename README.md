b9573d98e41f293bcdeb3e839b814ddddfe6fe08
Lucas Kroth <lucas.kroth@inova.local>
2022-09-02 17:52:21 -0300

    Implementada FindComPort() e ComCheck() - Inicio da estruturacao do objeto modbus

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Simulador de Temperatura Inova
 
+## Instalando
+
+Abra o terminal, e na pasta do script, execute:
+
+```
+npm i npm i @libs-scripts-mep/temp-sim
+```
+
+Após fianlizada a instalação da biblioteca, inclua em seu html:
+
+```html
+	<script src="node_modules/@libs-scripts-mep/temp-sim/temp-sim.js"></script>
+```
+
+# Comunicação
+
 ## Detalhes da Interface
 
 | Item      | Detalhe |
@@ -7,15 +23,88 @@
 | Interface | UART    |
 | Baud Rate | 9600    |
 | Data Bits | 8       |
-| Paridade  | Even    |
+| Paridade  | Par     |
 | Stop Bit  | 1       |
 
 ## Mapa de Registradores
 
-| Address | Descrição        | Estrutura de Dados | Observação |
-| ------- | ---------------- | ------------------ | ---------- |
-| 0x1E    | Tipo de Sensor   |                    |            |
-| 0x1F    | Modo de Operação |                    |            |
-| 0x20    | Valor            |                    |            |
-| 0x21    | Grupo            |                    |            |
+| Address | Tipo de Registrador | Descrição                             | Observação                                                         |
+| ------- | ------------------- | ------------------------------------- | ------------------------------------------------------------------ |
+| 0x1E    | Holding Register    | [Tipo de Sensor](#tipo-de-sensor)     | ***default*** Tipo J                                               |
+| 0x1F    | Holding Register    | [Modo de Operação](#modo-de-operação) |                                                                    |
+| 0x20    | Holding Register    | [Valor](#valor)                       | Tera efeito somente para [Modo de Operação](#modo-de-operação) = 0 |
+| 0x21    | Holding Register    | [Grupo](#grupo)                       |                                                                    |
 
+### Tipo de Sensor
+
+| Decimal | Hex  | Opção  |
+| ------- | ---- | ------ |
+| 0       | 0x00 | Tipo J |
+| 1       | 0x01 | Tipo K |
+| 2       | 0x02 | mV     |
+
+### Modo de Operação
+
+| Decimal | Hex  | Opção                     |
+| ------- | ---- | ------------------------- |
+| 0       | 0x00 | Output (Geração de Sinal) |
+| 1       | 0x01 | Input (Leitura de Sinal)  |
+
+### Valor
+
+| Decimal | Hex    | Opção                                                                                          |
+| ------- | ------ | ---------------------------------------------------------------------------------------------- |
+| 10      | 0x000A | Seta a temperatura em 10 graus, de acordo com o [Tipo de Sensor](#tipo-de-sensor) configurado  |
+| 300     | 0x012C | Seta a temperatura em 300 graus, de acordo com o [Tipo de Sensor](#tipo-de-sensor) configurado |
+| 750     | 0x02EE | Seta a temperatura em 750 graus, de acordo com o [Tipo de Sensor](#tipo-de-sensor) configurado |
+
+### Grupo
+
+| Decimal | Hex  | Opção     |
+| ------- | ---- | --------- |
+| 0       | 0x00 | Grupo A   |
+| 1       | 0x01 | Grupo B   |
+| 2       | 0x02 | Grupo C   |
+| 3       | 0x03 | Grupo D   |
+| 4       | 0x04 | Grupo E   |
+| ...     | 0x01 | Grupo ... |
+
+## Desmembrando a Requisição
+
+Tomando como exemplo a requisição:
+
+```01 10 00 1E 00 04 08 00 01 00 00 02 EE 00 00 EF 1F```
+
+Confira a estrutura do frame:
+
+| Byte | Significado                   | Descrição do Valor                                                |
+| ---- | ----------------------------- | ----------------------------------------------------------------- |
+| 0x01 | Node Address                  | Endereço na rede modbus                                           |
+| 0x10 | Modbus Function               | Write Multiple Registers                                          |
+| 0x00 | Start Address (High Byte)     | Endereço do primeiro registrador a ser lido                       |
+| 0x1E | Start Address (Low Byte)      | -                                                                 |
+| 0x00 | Nro os Registers (Hight Byte) | Quantidade de registradores para ler a partir do endereço inicial |
+| 0x04 | Nro os Registers (Low Byte)   | -                                                                 |
+| 0x08 | Byte Count                    | Indica número de bytes subsequentes desta requisição              |
+| 0x00 | Tipo de Sensor (High Byte)    | [Tipo de Sensor](#tipo-de-sensor)                                 |
+| 0x01 | Tipo de Sensor (Low Byte)     | -                                                                 |
+| 0x00 | Modo de Operação (High Byte)  | [Modo de Operação](#modo-de-operação)                             |
+| 0x00 | Modo de Operação (Low Byte)   | -                                                                 |
+| 0x02 | Valor (High Byte)             | [Valor](#valor)                                                   |
+| 0xEE | Valor (Low Byte)              | -                                                                 |
+| 0x00 | Grupo (High Byte)             | [Grupo](#grupo)                                                   |
+| 0x00 | Grupo (Low Byte)              | -                                                                 |
+| 0xEF | CRC (High Byte)               | Ciclic Redundancy Check                                           |
+| 0x1F | CRC (Low Byte)                | -                                                                 |
+
+
+### Cola
+
+| Tipo de Sensor | Modo de Operação | Valor | Requisição                                         |
+| -------------- | ---------------- | ----- | -------------------------------------------------- |
+| Tipo J         | Output           | 10°C  | 01 10 00 1E 00 04 08 00 00 00 00 00 0A 00 00 BE 50 |
+| Tipo J         | Output           | 300°C | 01 10 00 1E 00 04 08 00 00 00 00 01 2C 00 00 5E 67 |
+| Tipo J         | Output           | 750°C | 01 10 00 1E 00 04 08 00 00 00 00 02 EE 00 00 FF DF |
+| Tipo K         | Output           | 10°C  | 01 10 00 1E 00 04 08 00 01 00 00 00 0A 00 00 AE 90 |
+| Tipo K         | Output           | 300°C | 01 10 00 1E 00 04 08 00 01 00 00 01 2C 00 00 4E A7 |
+| Tipo K         | Output           | 750°C | 01 10 00 1E 00 04 08 00 01 00 00 02 EE 00 00 EF 1F |
--- temp-sim.js
+++ temp-sim.js
@@ -1,54 +1,169 @@
-class CappoInova {
+class SimuladorTemp {
 
-    constructor(context) {
-        this.context = context
+    constructor(baudRate = 9600, paridade = 2) {
 
+        this.Serial = new SerialPVI(baudRate, paridade) //temporario
+        this.RegexID = new RegExp("[0][1][ ][1][0][ ][0][0][ ][1][E][ ][0][0][ ][0][4][ ][A][1][ ][C][C]") //temporario
+        this.ReqID = "01 10 00 1E 00 04 08 00 01 00 00 01 2C 00 00 4E A7"
+        this.StatusCom = false
 
+        this.Modbus = {
+            Address: {
+                HoldingRegister: {
+                    TipoSensor: "00 1E",
+                    ModoOperacao: "00 1F",
+                    Valor: "00 20",
+                    Grupo: "00 21"
+                },
+                InputRegister: {
+
+                }
+            }
+        }
+        this.OutputConfig = {
+            TipoSensor: "00 00",
+            ModoOperacao: "00 00",
+            Valor: "00 00",
+            Grupo: "00 00"
+        }
 
     }
 
-    send(sensor = 0, valor = 350, entSai = 0, grupo = 1, callback, out = 5) {
-        setTimeout(() => {
-            let Send = new Map()
-            Send.set("node", "01")
-            Send.set("command", "10")
-            Send.set("RegistrInicial", "00 1E")
-            Send.set("NumRegist", "00 04")
-            Send.set("NumBytes", "08")
-            Send.set("TipoSensor", this.context.serial.DecimalToHex(sensor))
-            Send.set("ModoOP", this.context.serial.DecimalToHex(entSai))
-            Send.set("Valor", this.context.serial.DecimalToHex(valor))
-            Send.set("Grupo", this.context.serial.DecimalToHex(grupo))
-            var strReq = this.context.serial.geraStringReq(Send)
+    /**
+     * 
+     * @param {function} callback 
+     */
+    FindComPort(callback) {
 
-            let serialData = ""
-            let retries = 0
-            var ItCappo = setInterval(() => {
-                this.context.serial.SendData(strReq)
-                setTimeout(() => {
-                    serialData += this.context.serial.ReadData()
-                    console.log("data cappo resp ", serialData)
-                    if (retries <= out) {
-                        if (serialData.includes("01 10 00 1E 00 04 A1 CC")) {
-                            clearInterval(ItCappo)
-                            callback(true)
-                        } else {
-                            retries++
-                        }
-                    } else {
-                        clearInterval(ItCappo)
-                        callback(false)
-                    }
-                }, 500)
-            }, 2000)
+        SerialPVI.closeAllPorts()
+
+        this.Serial.getConnectedPortCom(this.ReqID, this.RegexID, (result, COMPort) => {
+
+            if (result) {
+                this.StatusCom = true
+            }
+
+            callback(result, COMPort)
 
         }, 500)
-
     }
-}
 
-class SimuladorTemp {
-    constructor() {
-        this.Serial = new Serial()
+    /**
+     * 
+     * @param {number} timeOut 
+     */
+    ComCheck(timeOut = 300) {
+
+        this.Serial.SendData(this.ReqID, this.Serial.COMPORT)
+
+        setTimeout(() => {
+
+            let byteData = this.Serial.ReadData(this.Serial.COMPORT).match(this.RegexID)
+
+            if (byteData != null) {
+
+                this.StatusCom = true
+                return true
+
+            } else {
+
+                this.StatusCom = false
+                return false
+
+            }
+        }, timeOut)
+    }
+
+
+    SetConfig(sensor, modoOp, temp, grp, callback) {
+
+        switch (modoOp) {
+
+            case "Output":
+                this.OutputConfig.ModoOperacao = "00 00"
+
+                switch (sensor) {
+                    case "J":
+
+                        if (!isNaN(temp) && temp >= 10 && temp <= 750) {
+
+                            this.OutputConfig.Valor = SerialPVI.DecimalToHex(temp)
+                            this.OutputConfig.TipoSensor = "00 00"
+                            callback(false, "Nova Configuração Recebida")
+
+                        } else {
+                            callback(false, "Valor fora do range")
+                        }
+
+                        break
+
+                    case "K":
+                        if (!isNaN(temp) && temp >= 10 && temp <= 750) {
+
+                            this.OutputConfig.Valor = SerialPVI.DecimalToHex(temp)
+                            this.OutputConfig.TipoSensor = "00 01"
+                            callback(false, "Nova Configuração Recebida")
+
+                        } else {
+                            callback(false, "Valor fora do range")
+                        }
+                        break
+
+                    case "mV":
+                        if (!isNaN(temp) && temp >= 10 && temp <= 750) {
+
+                            this.OutputConfig.Valor = SerialPVI.DecimalToHex(temp)
+                            this.OutputConfig.TipoSensor = "00 02"
+                            callback(false, "Nova Configuração Recebida")
+
+                        } else {
+                            callback(false, "Valor fora do range")
+                        }
+                        break
+
+                    default:
+                        callback(false, "Sensor Inválido")
+                        break
+                }
+
+                switch (grp) {
+                    case "A":
+                        this.OutputConfig.Grupo = "00 00"
+                        break
+
+                    case "B":
+                        this.OutputConfig.Grupo = "00 01"
+                        break
+
+                    case "C":
+                        this.OutputConfig.Grupo = "00 02"
+                        break
+
+                    case "D":
+                        this.OutputConfig.Grupo = "00 03"
+                        break
+
+                    case "E":
+                        this.OutputConfig.Grupo = "00 04"
+                        break
+
+                    default:
+                        this.OutputConfig.Grupo = "00 00"
+                        break
+                }
+                break
+
+            case "Input":
+                this.OutputConfig.ModoOperacao = "00 01"
+                break
+
+            default:
+                callback(false, "Modo Inválido")
+                break
+        }
+    }
+
+    SendConfig(config = this.OutputConfig) {
+
     }
 }
